4a3c46706ee3c0c59ec34f2f9111f4de2db4c21b
deivisonisidoro <deivisonisidoro@gmail.com>
2022-03-06 14:43:52 -0300

    feat: add documentation for CRUD

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,15 @@ import "express-async-errors";
 import { routes } from "./routes";
 import "./database";
 import "dotenv/config";
+import swaggerUi from'swagger-ui-express';
+import swaggerDocs from './swagger.json';
+
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(routes);
 
